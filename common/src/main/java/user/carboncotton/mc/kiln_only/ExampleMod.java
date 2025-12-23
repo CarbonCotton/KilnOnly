a3b45d7fd0962dd4e710a55de0fd8226101166ad
CarbonCotton <CarbonCotton@proton.me>
2025-12-23 23:24:10 +0100

    feat: Adding project template

---
+++ common/src/main/java/user/carboncotton/mc/kiln_only/ExampleMod.java
@@ -0,0 +1,9 @@
+package user.carboncotton.mc.kiln_only;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "kiln_only";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
